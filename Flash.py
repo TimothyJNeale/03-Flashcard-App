1bb403001a7f99b5b1761eaac759712bbb55cc5d
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-14 11:40:34 +0100

    Added function calls for menu commands

--- Flash.py
+++ Flash.py
@@ -7,20 +7,41 @@ root.iconbitmap("01 Nenebiker.ico")
 
 # Create function to add two numbers
 def add():
-    pass
+    hide_all_frames()
+    add_frame.pack(fill="both", expand=1)
 
 # Create function to subtract two numbers
 def subtract():
-    pass
+    hide_all_frames()
+    subtract_frame.pack(fill="both", expand=1)
 
 # Create function to multiply two numbers
 def multiply():
-    pass
+    hide_all_frames()
+    multiply_frame.pack(fill="both", expand=1)
 
 # Create function to divide two numbers
 def divide():
-    pass
+    hide_all_frames()
+    divide_frame.pack(fill="both", expand=1)  
 
+# Hide menu frames
+def hide_all_frames():
+    # Loop thru and hide all frames
+    for widget in add_frame.winfo_children():
+        widget.pack_forget()
+    for widget in subtract_frame.winfo_children():
+        widget.pack_forget()
+    for widget in multiply_frame.winfo_children():
+        widget.pack_forget()
+    for widget in divide_frame.winfo_children():
+        widget.pack_forget()
+
+    # Hide all frames
+    add_frame.pack_forget()
+    subtract_frame.pack_forget()
+    multiply_frame.pack_forget()
+    divide_frame.pack_forget()
 
 # Define main menu
 main_menu = Menu(root)
@@ -35,6 +56,13 @@ math_menu.add_command(label="Multiply", command=multiply)
 math_menu.add_command(label="Divide", command=divide)
 math_menu.add_separator()
 math_menu.add_command(label="Exit", command=root.quit)
+
+# Create Math Frames
+add_frame = Frame(root, width=500, height=500, bg="blue")
+subtract_frame = Frame(root, width=500, height=500, bg="red")
+multiply_frame = Frame(root, width=500, height=500, bg="green")
+divide_frame = Frame(root, width=500, height=500, bg="yellow")
+
 
 
 
