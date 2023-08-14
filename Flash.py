05de28c19aa96bc6a6b6eeb2d220d99d203e01f0
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-14 16:00:05 +0100

    Add frame completed

--- Flash.py
+++ Flash.py
@@ -6,27 +6,57 @@ root.title("Maths Flashcards")
 root.geometry("500x500")
 root.iconbitmap("01 Nenebiker.ico")
 
+
 # Create function to add two numbers
 def add():
     hide_all_frames()
     add_frame.pack(fill="both", expand=1)
 
     # Create two random numbers
-    num_1 = randint(0, 10)
-    num_2 = randint(0, 10)
+    global num_1
+    global num_2
+    num_1 = IntVar()
+    num_2 = IntVar()
+
+    num_1.set(randint(0, 10))
+    num_2.set(randint(0, 10))
 
     # Put random numbers on screen
-    add_label = Label(add_frame, text=f"{num_1} + {num_2}", font=("helvetica", 32))
+    global add_label
+    add_label = Label(add_frame, text=f"{num_1.get()} + {num_2.get()}", font=("helvetica", 32))
     add_label.pack(pady=20, padx=50)
 
     # Create answer box
+    global add_answer
     add_answer = Entry(add_frame, font=("helvetica", 18))
     add_answer.pack(pady=10, padx=50)
 
     # Create submit button
-    add_button = Button(add_frame, text="Answer", command="")
+    add_button = Button(add_frame, text="Answer", command=lambda: add_answer_func(num_1.get(), num_2.get(), add_answer.get()))
     add_button.pack(pady=10)
 
+    global answer_label
+    answer_label = Label(add_frame, text="Enter the correct value above", font=("helvetica", 18))
+    answer_label.pack(pady=10)
+
+# Create function to check add answer
+def add_answer_func(num1, num2, answer):
+    # Calculate the answer
+    correct_answer = num1 + num2
+
+    # Check if answer is correct
+    if correct_answer == int(answer):
+        answer_label.config(text="Correct Answer! " + str(num1) + " + " + str(num2) + " = " + str(correct_answer))
+    else:
+        answer_label.config(text="Incorrect Answer! " + str(num1) + " + " + str(num2) + " = " + str(correct_answer) + " not " + str(answer))
+
+    # Clear the answer box
+    add_answer.delete(0, "end")
+
+    # Generate two new random numbers
+    num_1.set(randint(0, 10))
+    num_2.set(randint(0, 10))
+    add_label.config(text=f"{num_1.get()} + {num_2.get()}")
 
 # Create function to subtract two numbers
 def subtract():
